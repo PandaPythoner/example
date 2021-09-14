d653b5bb32a53738aa4125c193d5610b5f759f30
pandapythoner <pandapythoner@gmail.com>
2021-09-14 20:54:23 +0300

    Added party ask to program

--- main.cpp
+++ main.cpp
@@ -4,23 +4,31 @@
 #include <algorithm>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 
 using namespace std;
+
+
+/*
+Navalny is the best!
+*/
 
 
 int32_t main(){
 	ofstream file;
 	file.open("users_data.txt", ios_base::app);
 	cout << "Hellow, user! We need some data about you." << "\n";
-	string name1, name2, book;
+	string name1, name2, book, party;
 	cout << "First name: ";
-	cin >> name1;
+	getline(cin, name1, '\n');
 	cout << "Second name: ";
-	cin >> name2;
+	getline(cin, name2, '\n');
 	cout << "Your favorite book: ";
-	cin >> book;
-	file << name1 << " " << name2 << " " << book << "\n";
+	getline(cin, book, '\n');
+	cout << "Which party will you vote for: ";
+	getline(cin, party, '\n');
+	file << name1 << " " << name2 << " '" << book << "' '" << party << "'\n";
 	file.close();
 	return 0;
 }
