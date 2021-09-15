1de2e28790516e7bd6032101dbb32df994e67e00
pandapythoner <pandapythoner@gmail.com>
2021-09-15 10:11:42 +0300

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -9,9 +9,14 @@
 using namespace std;
 
 
+string users_data_filename = "users_data.txt";
+
+
+
 int32_t main(){
+	print_users_data();
 	ofstream file;
-	file.open("users_data.txt", ios_base::app);
+	file.open(users_data_filename.data(), ios_base::app);
 	cout << "Hellow, user! We need some data about you." << "\n";
 	string name1, name2, book;
 	cout << "First name: ";
@@ -20,7 +25,7 @@ int32_t main(){
 	cin >> name2;
 	cout << "Your favorite book: ";
 	cin >> book;
-	file << name1 << " " << name2 << " " << book << "\n";
+	file << name1 << "; " << name2 << "; " << book << ";\n";
 	file.close();
 	return 0;
 }
