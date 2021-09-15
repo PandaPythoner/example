513eaec786b67f2d02472f7c4f7b1a8a905f55e3
pandapythoner <pandapythoner@gmail.com>
2021-09-15 10:39:10 +0300

    Now asks are in file asks.txt

--- main.cpp
+++ main.cpp
@@ -10,22 +10,34 @@ using namespace std;
 
 
 string users_data_filename = "users_data.txt";
+string asks_data_filename = "asks.txt";
 
+
+vector<string> asks;
+
+
+void get_asks() {
+	ifstream file;
+	file.open(asks_data_filename.data());
+	string ask;
+	asks.clear();
+	while (getline(file, ask)) {
+		asks.push_back(ask);
+	}
+}
 
 
 int32_t main(){
-	print_users_data();
+	get_asks();
 	ofstream file;
 	file.open(users_data_filename.data(), ios_base::app);
-	cout << "Hellow, user! We need some data about you." << "\n";
-	string name1, name2, book;
-	cout << "First name: ";
-	cin >> name1;
-	cout << "Second name: ";
-	cin >> name2;
-	cout << "Your favorite book: ";
-	cin >> book;
-	file << name1 << "; " << name2 << "; " << book << ";\n";
+	vector<string> answers(asks.size());
+	for (int i = 0; i < asks.size(); ++i) {
+		cout << asks[i];
+		getline(cin, answers[i]);
+		file << answers[i] << ';';
+	}
+	file << '\n';
 	file.close();
 	return 0;
 }
